038075aa6c4cbbcbefa9eb994a6ee8a794c14a97
Adam Zinder and Luke Philips <luke@>
2016-11-16 16:21:10 -0800

    case support

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -1,13 +1,14 @@
 class String
   define_method(:find_replace) do |find_term, replace_term|
-    self.gsub!(find_term, replace_term) #<------For partial matches
-
-    # array_words = self.split()   <------ For word matches
-    # array_words.each() do |word|
-    #   if word == find_term
-    #     word = word.replace(replace_term)
-    #   end
-    # end
-    # array_words.join(" ")
+    self.downcase().gsub!(find_term.downcase(), replace_term)
   end
 end
+
+  # define_method(:find_replace_all) do |find_term, replace_term|
+  #   array_words = self.split()
+  #   array_words.each() do |word|
+  #     if word == find_term
+  #       word = word.replace(replace_term)
+  #     end
+    # array_words.join(" ")
+    # end
